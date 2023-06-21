bf6d8434a8f2171ad5c438eb05a5821188441d1e
박영철 <parkdigy@gmail.com>
2023-06-21 15:24:18 +0900

    1.0.25

--- src/FormItemTextFieldBase/FormHidden/FormHidden.types.ts
+++ src/FormItemTextFieldBase/FormHidden/FormHidden.types.ts
@@ -1,7 +1,10 @@
 import { FormTextProps } from '../FormText';
 import { FormValueItemBaseCommands } from '../../@types';
 
-export type FormHiddenProps = Pick<FormTextProps, 'className' | 'name' | 'value'>;
+export type FormHiddenProps = Pick<
+  FormTextProps,
+  'className' | 'name' | 'value' | 'error' | 'required' | 'exceptValue' | 'onChange' | 'onValidate'
+>;
 
 export const FormHiddenDefaultProps = {};
 
